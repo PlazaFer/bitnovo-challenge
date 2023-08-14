d97e2d39bd4c037381c751d527f2aefe9df74b3e
Fernando Plaza <fernando.plaza@itrocksweb.com>
2023-08-13 23:15:10 -0300

    BT-2 - Add new font and config theme in tailwind

--- App.tsx
+++ App.tsx
@@ -1,10 +1,30 @@
+import { useEffect } from 'react';
 import { SafeAreaView, Text, View } from 'react-native';
+import * as Font from "expo-font";
 
 export default function App() {
+
+  useEffect(() => {
+    loadFont();
+  }, []);
+
+
+  const loadFont = async () => {
+    try {
+      await Font.loadAsync({
+        "Mulish-Bold": require("./fonts/Mulish-Bold.ttf"),
+        "Mulish-Regular": require("./fonts/Mulish-Regular.ttf"),
+        "Mulish-SemiBold": require("./fonts/Mulish-SemiBold.ttf")
+      });
+    } catch (e) {
+      console.warn(e)
+    }
+  }
+
   return (
     <SafeAreaView>
       <View>
-        <Text>Open up App.tsx to start working on your app!</Text>
+        <Text>App.tsx</Text>
       </View>
     </SafeAreaView>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,34 @@ module.exports = {
     "./App.tsx"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        'xxl': '40px',
+        'xl': '25px',
+        'lg': '16px',
+        'sm': '14px',
+      },
+      fontFamily: {
+        'mulish': ['Mulish', 'sans']
+      },
+      colors: {
+        blue: {
+          100: "#035AC5",
+          800: "#002859"
+        },
+        gray: {
+          100: "#D3DCE6",
+          800: "#758192"
+        },
+        red: {
+          800: "#B91C1C"
+        }
+      },
+      borderRadius: {
+        sm: "6px",
+        lg: "24px"
+      }
+    },
   },
   plugins: [],
 }
